73d9ea3bc8a7dbedef107591dcd2fbded895b151
Valentine-Yeskov <valentineyescov@gmail.com>
2023-05-26 13:24:28 +0300

    two classes add

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,15 @@ import dogsClass.Dog;
 import dogsClass.Puppy1;
 import dogsClass.Puppy2;
 import dogsClass.Puppy3;
+
+import carClass.Vag;
+import carClass.Volkswagen;
+import carClass.Passat;
+import carClass.Touareg;
+import carClass.Golf;
+
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal() {
-            @Override
-            public void saySomething() {
-                super.saySomething();
-            }
-        };
 
         Puppy3 puppy3 = new Puppy3();
         puppy3.sayGaf();
@@ -21,14 +22,24 @@ public class Main {
 
         Dog dog = new Puppy1();
         dog.saySomething();
-
         dog = new Puppy2();
         dog.saySomething();
-
         dog = new Puppy3();
         dog.saySomething();
 
+        Golf golf = new Golf();
+        golf.SayIamGay();
+        Passat passat = new Passat();
+        passat.sayBestCarForOldPeople();
+        Touareg touareg = new Touareg();
+        touareg.sayImMan();
 
+        Volkswagen volkswagen = new Golf();
+        volkswagen.sayIamSold();
+        volkswagen = new Passat();
+        volkswagen.sayIamSold();
+        volkswagen = new Touareg();
+        volkswagen.sayIamSold();
     }
 
 }
