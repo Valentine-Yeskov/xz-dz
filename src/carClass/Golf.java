5cf9461f062c82bfa482f18b3353c2d155c0fd8e
Valentine-Yeskov <valentineyescov@gmail.com>
2023-05-26 19:17:02 +0300

    без Base b

--- src/carClass/Golf.java
+++ src/carClass/Golf.java
@@ -1,6 +1,8 @@
 package carClass;
 
-final public class Golf extends Volkswagen{
+import interfacesClass.Move;
+
+final public class Golf extends Volkswagen implements Move {
 
     public String gender;
     public int age;
@@ -13,4 +15,23 @@ final public class Golf extends Volkswagen{
         System.out.println("I am Golf and i am the best car in the wold!");
     }
 
+    @Override
+    public void down() {
+        System.out.println("Move  Down");
+    }
+
+    @Override
+    public void up() {
+        System.out.println("Move Up");
+    }
+
+    @Override
+    public void left() {
+        System.out.println("Turn Right");
+    }
+
+    @Override
+    public void right() {
+        System.out.println("Turn Right");
+    }
 }
--- src/dogsClass/Puppy3.java
+++ src/dogsClass/Puppy3.java
@@ -1,5 +1,8 @@
 package dogsClass;
-final public class Puppy3 extends Dog{
+import interfacesClass.Power;
+import interfacesClass.Volume;
+
+final public class Puppy3 extends Dog implements Volume {
     public int birthDay;  //year
     public String master;
     public void saySomething() {
@@ -7,5 +10,24 @@ final public class Puppy3 extends Dog{
     }
     public void sayGaf() {
         System.out.println("Grrrrrrrr, Grrrrrrrr");
+    }
+    @Override
+    public void on(){
+        System.out.println("On");
+    }
+
+    @Override
+    public void off() {
+        System.out.println("Off");
+    }
+
+    @Override
+    public void volumeUp() {
+        System.out.println("Volume Up");
+    }
+
+    @Override
+    public void volumeDown() {
+        System.out.println("Volume Down");
     }
 }
